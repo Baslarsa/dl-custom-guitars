417b8960b241c148b4b9b406b5d7614e7a7e75fd
Baslarsa <davvelars@gmail.com>
2024-10-26 13:21:43 +0200

    fix revalidate

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -10,10 +10,7 @@ export const repositoryName =
 
 /**
  * A list of Route Resolver objects that define how a document's `url` field is resolved.
- *
- * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
-// TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig["routes"] = [
   {
     type: "home_page",
@@ -26,18 +23,16 @@ const routes: prismic.ClientConfig["routes"] = [
 ];
 
 /**
- * Creates a Prismic client for the project's repository. The client is used to
- * query content from the Prismic API.
+ * Creates a Prismic client for the project's repository.
  *
  * @param config - Configuration for the Prismic client.
  */
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+    fetchOptions: {
+      next: { tags: ["prismic"], revalidate: 5 },
+    },
     ...config,
   });
 
