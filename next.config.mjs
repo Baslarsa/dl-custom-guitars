e16c4f7779d76522028b5e8ee0dfa7584a595fc3
Baslarsa <davvelars@gmail.com>
2024-11-07 14:25:12 +0100

    add analytics to csm

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
               "default-src 'self'; " +
               "frame-src https://dlguitars.prismic.io https://dlgutars.prismic.io https://www.googletagmanager.com; " +
               "script-src 'self' https://static.cdn.prismic.io https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval'; " +
-              "connect-src 'self' https://dlguitars.prismic.io https://www.google-analytics.com https://www.googletagmanager.com; " +
+              "connect-src 'self' https://dlguitars.prismic.io https://www.google-analytics.com https://www.googletagmanager.com https://region1.google-analytics.com; " +
               "img-src 'self' https://images.prismic.io https://www.googletagmanager.com https://www.google-analytics.com; " +
               "style-src 'self' 'unsafe-inline';",
           },
