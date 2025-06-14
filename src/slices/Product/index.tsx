ef8f6b31725e10aadcfceaf31261594758309a44
Baslarsa <davvelars@gmail.com>
2025-06-14 21:23:34 +0300

    update image gallery

--- src/slices/Product/index.tsx
+++ src/slices/Product/index.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import classNames from "classnames";
-
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 import {
   ProductDocument,
   ProductDocumentData,
@@ -9,6 +13,15 @@ import {
 } from "../../../prismicio-types";
 import { PrismicNextImage } from "@prismicio/next";
 import RichText from "@/app/components/typography/RichText";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 /**
  * Props for `Product`.
@@ -24,7 +37,14 @@ const Product = ({
   data: Simplify<ProductDocumentData>;
 }): JSX.Element => {
   const product = data;
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  const handleImageClick = (index: number) => {
+    setOpen(true);
+    setActiveIndex(index);
+  };
   return (
     <div className="bg-black text-white">
       <div className="pb-16 pt-6 sm:pb-24">
@@ -39,23 +59,56 @@ const Product = ({
             {/* Image gallery */}
             <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
               <h2 className="sr-only">Images</h2>
-
-              <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-3 lg:gap-8">
-                {product.images?.map((item) => (
-                  <PrismicNextImage
-                    key={item.image.url}
-                    field={item.image}
-                    className={classNames(
-                      product.images[0]?.image.id === item.image.id
-                        ? "lg:col-span-2 lg:row-span-1"
-                        : "hidden lg:block",
-                      "rounded-lg w-full object-cover object-center"
-                    )}
-                  />
+              <Swiper
+                spaceBetween={10}
+                thumbs={{ swiper: thumbsSwiper }}
+                modules={[FreeMode, Thumbs]}
+              >
+                {product.images?.map((item, index) => (
+                  <SwiperSlide>
+                    <div className="w-full h-[500px]">
+                      <PrismicNextImage
+                        key={item.image.url}
+                        field={item.image}
+                        onClick={() => handleImageClick(index)}
+                        className={classNames(
+                          "rounded-lg w-full h-full object-contain object-center cursor-pointer"
+                        )}
+                      />
+                    </div>
+                  </SwiperSlide>
                 ))}
-              </div>
+              </Swiper>
+              <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mySwiper"
+              >
+                {product.images?.map((item, index) => (
+                  <SwiperSlide>
+                    <div className="w-full h-36 cursor-pointer">
+                      <PrismicNextImage
+                        key={item.image.url}
+                        field={item.image}
+                        className="rounded-lg w-full h-full object-cover object-center"
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
-
+            <Lightbox
+              open={open}
+              close={() => setOpen(false)}
+              slides={product.images?.map((item) => ({
+                src: item.image.url,
+              }))}
+              index={activeIndex}
+            />
             <div className="mt-8 lg:col-span-5">
               {/* Product details */}
               <div className="">
